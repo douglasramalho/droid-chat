25ca42a1d054c219f1bde453897587c77a4a861d
Douglas Motta <douglas1102rm@hotmail.com>
2024-07-18 21:15:36 +0100

    Módulo 02 - Aula: Implementando a versão final do componente: PrimaryTextField

--- app/src/main/java/com/example/droidchat/ui/feature/signin/SignInScreen.kt
+++ app/src/main/java/com/example/droidchat/ui/feature/signin/SignInScreen.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -14,6 +16,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.droidchat.R
@@ -40,6 +44,8 @@ fun SignInScreen() {
             contentDescription = null
         )
 
+        Spacer(modifier = Modifier.height(64.dp))
+
         var email by remember {
             mutableStateOf("")
         }
@@ -50,7 +56,28 @@ fun SignInScreen() {
                 email = it
             },
             modifier = Modifier
-                .padding(horizontal = 16.dp)
+                .padding(horizontal = 16.dp),
+            placeholder = stringResource(id = R.string.feature_login_email),
+            leadingIcon = R.drawable.ic_envelope,
+            keyboardType = KeyboardType.Email,
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        var password by remember {
+            mutableStateOf("")
+        }
+
+        PrimaryTextField(
+            value = password,
+            onValueChange = {
+                password = it
+            },
+            modifier = Modifier
+                .padding(horizontal = 16.dp),
+            placeholder = stringResource(id = R.string.feature_login_password),
+            leadingIcon = R.drawable.ic_lock,
+            keyboardType = KeyboardType.Password,
         )
     }
 }
