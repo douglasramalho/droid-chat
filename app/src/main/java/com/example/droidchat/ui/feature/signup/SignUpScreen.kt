6dea7c5270fe63504d925e7101fab1f99808ccad
Douglas Motta <douglas1102rm@hotmail.com>
2024-09-21 11:03:33 +0100

    Módulo 02 - Aula: Implementando captura de foto pelo aplicativo de câmera do smartphone

--- app/src/main/java/com/example/droidchat/ui/feature/signup/SignUpScreen.kt
+++ app/src/main/java/com/example/droidchat/ui/feature/signup/SignUpScreen.kt
@@ -3,6 +3,7 @@ package com.example.droidchat.ui.feature.signup
 import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -12,12 +13,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -29,16 +33,19 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.droidchat.R
 import com.example.droidchat.ui.components.PrimaryButton
+import com.example.droidchat.ui.components.ProfilePictureOptionsModalBottomSheet
 import com.example.droidchat.ui.components.ProfilePictureSelector
 import com.example.droidchat.ui.components.SecondaryTextField
 import com.example.droidchat.ui.theme.BackgroundGradient
 import com.example.droidchat.ui.theme.DroidChatTheme
+import kotlinx.coroutines.launch
 
 @Composable
 fun SignUpRoute() {
     SignUpScreen()
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SignUpScreen() {
     Box(
@@ -55,6 +62,10 @@ fun SignUpScreen() {
 
             var profilePictureSelectedUri by remember {
                 mutableStateOf<Uri?>(null)
+            }
+
+            var openProfilePictureOptionsModalBottomSheet by remember {
+                mutableStateOf(false)
             }
 
             Image(
@@ -79,7 +90,11 @@ fun SignUpScreen() {
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
                     ProfilePictureSelector(
-                        imageUri = profilePictureSelectedUri
+                        imageUri = profilePictureSelectedUri,
+                        modifier = Modifier
+                            .clickable {
+                                openProfilePictureOptionsModalBottomSheet = true
+                            }
                     )
 
                     Spacer(modifier = Modifier.height(30.dp))
@@ -133,6 +148,25 @@ fun SignUpScreen() {
                         onClick = {}
                     )
                 }
+            }
+
+            val sheetState = rememberModalBottomSheetState()
+            val scope = rememberCoroutineScope()
+            if (openProfilePictureOptionsModalBottomSheet) {
+                ProfilePictureOptionsModalBottomSheet(
+                    onPictureSelected = { uri ->
+                        profilePictureSelectedUri = uri
+                        scope.launch {
+                            sheetState.hide()
+                        }.invokeOnCompletion {
+                            if (!sheetState.isVisible) {
+                                openProfilePictureOptionsModalBottomSheet = false
+                            }
+                        }
+                    },
+                    onDismissRequest = { openProfilePictureOptionsModalBottomSheet = false },
+                    sheetState = sheetState,
+                )
             }
         }
     }
