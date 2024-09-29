7b3e9505bd4e78fc0b409468817c37852147fb38
Douglas Motta <douglas1102rm@hotmail.com>
2024-09-29 14:17:41 +0100

    Módulo 02 - Aula: Criando o SignUpViewModel e gerenciando o estado da tela de SignUp

---
+++ app/src/main/java/com/example/droidchat/ui/feature/signup/SignUpFormState.kt
@@ -0,0 +1,21 @@
+package com.example.droidchat.ui.feature.signup
+
+import android.net.Uri
+import androidx.annotation.StringRes
+
+data class SignUpFormState(
+    val profilePictureUri: Uri? = null,
+    val firstName: String = "",
+    @StringRes val firstNameError: Int? = null,
+    val lastName: String = "",
+    @StringRes val lastNameError: Int? = null,
+    val email: String = "",
+    @StringRes
+    val emailError: Int? = null,
+    val password: String = "",
+    @StringRes
+    val passwordError: Int? = null,
+    val passwordConfirmation: String = "",
+    val isProfilePictureModalBottomSheetOpen: Boolean = false,
+    val isLoading: Boolean = false,
+)
