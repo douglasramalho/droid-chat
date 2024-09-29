5c3a8a131cc0d4d0b3f2ebd45c584d5572cbb826
Douglas Motta <douglas1102rm@hotmail.com>
2024-09-29 13:10:54 +0100

    Módulo 02 - Aula: Implementando captura de foto pelo aplicativo de câmera do smartphone

--- app/src/main/java/com/example/droidchat/MainActivity.kt
+++ app/src/main/java/com/example/droidchat/MainActivity.kt
@@ -3,14 +3,10 @@ package com.example.droidchat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.droidchat.navigation.ChatNavHost
 import com.example.droidchat.ui.ChatApp
 import com.example.droidchat.ui.theme.DroidChatTheme
 
