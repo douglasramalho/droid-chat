b8212c4d39ec2694e19f115bb55305ab1362139c
Douglas Motta <douglas1102rm@hotmail.com>
2024-10-25 17:38:17 +0100

    Módulo 02 - Aula: Ajustes finais do módulo 2

--- app/src/main/java/com/example/droidchat/MainActivity.kt
+++ app/src/main/java/com/example/droidchat/MainActivity.kt
@@ -3,10 +3,7 @@ package com.example.droidchat
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.enableEdgeToEdge
 import com.example.droidchat.ui.ChatApp
 import com.example.droidchat.ui.theme.DroidChatTheme
 
@@ -14,26 +11,11 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             DroidChatTheme {
                 ChatApp()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DroidChatTheme {
-        Greeting("Android")
     }
 }
