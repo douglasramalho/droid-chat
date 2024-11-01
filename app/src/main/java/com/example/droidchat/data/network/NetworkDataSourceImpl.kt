09a74bb3e8b5d634199c42e808af6aa8a54469d9
Douglas Motta <douglas1102rm@hotmail.com>
2024-11-01 21:10:41 +0000

    Módulo 03 - Aula: Implementando o NetworkDataSource

---
+++ app/src/main/java/com/example/droidchat/data/network/NetworkDataSourceImpl.kt
@@ -0,0 +1,28 @@
+package com.example.droidchat.data.network
+
+import com.example.droidchat.data.network.model.AuthRequest
+import com.example.droidchat.data.network.model.CreateAccountRequest
+import com.example.droidchat.data.network.model.TokenResponse
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import javax.inject.Inject
+
+class NetworkDataSourceImpl @Inject constructor(
+    private val httpClient: HttpClient
+) : NetworkDataSource {
+
+    override suspend fun signUp(request: CreateAccountRequest) {
+        httpClient.post("signup") {
+            setBody(request)
+        }.body<Unit>()
+    }
+
+    override suspend fun signIn(request: AuthRequest): TokenResponse {
+        return httpClient.post("signin") {
+            setBody(request)
+        }.body()
+    }
+
+}
