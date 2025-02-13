f30272f15695ef2dc8709d9290c24cdf131c6fac
Douglas Motta <douglas1102rm@hotmail.com>
2025-02-13 23:18:54 +0000

    Módulo 04 - Aula: Finalizando a UI da tela de Usuários e gerenciamento dos estados de Loading, Success e Error com o Paging

--- app/src/main/java/com/example/droidchat/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/example/droidchat/data/repository/UserRepositoryImpl.kt
@@ -3,13 +3,14 @@ package com.example.droidchat.data.repository
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import androidx.paging.PagingSource
+import com.example.droidchat.data.network.NetworkDataSource
+import com.example.droidchat.data.pagingsource.UserPagingSource
 import com.example.droidchat.model.User
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
-    private val userPagingSource: PagingSource<Int, User>
+    private val networkDataSource: NetworkDataSource,
 ) : UserRepository {
 
     override fun getUsers(limit: Int): Flow<PagingData<User>> {
@@ -18,7 +19,7 @@ class UserRepositoryImpl @Inject constructor(
                 pageSize = limit,
                 enablePlaceholders = false,
             ),
-            pagingSourceFactory = { userPagingSource }
+            pagingSourceFactory = { UserPagingSource(networkDataSource) }
         ).flow
     }
 }
