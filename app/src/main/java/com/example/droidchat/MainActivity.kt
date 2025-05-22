4bcaf831efb990cc162913a2018d0c9c0da99ed4
Douglas Motta <douglas1102rm@hotmail.com>
2025-05-22 21:14:06 +0100

    Módulo 05 - Aula: Ajuste definitivo sobre edge to edge no Android 15+

--- app/src/main/java/com/example/droidchat/MainActivity.kt
+++ app/src/main/java/com/example/droidchat/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.droidchat
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -19,9 +18,8 @@ class MainActivity : ComponentActivity() {
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        Log.d("MainActivity", "onCreate: ${intent.data}")
+        super.onCreate(savedInstanceState)
         setContent {
             DroidChatTheme {
                 val navigationState = rememberDroidChatNavigationState()
--- app/src/main/java/com/example/droidchat/ui/ChatApp.kt
+++ app/src/main/java/com/example/droidchat/ui/ChatApp.kt
@@ -1,9 +1,9 @@
 package com.example.droidchat.ui
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -27,13 +27,13 @@ fun ChatApp(navigationState: DroidChatNavigationState) {
             }
         },
         containerColor = Grey1,
+        contentWindowInsets = WindowInsets(0, 0, 0, 0)
     ) { innerPaddings ->
         Box(
            modifier = Modifier
-               .consumeWindowInsets(innerPaddings)
-               .padding(innerPaddings)
-               .imePadding()
                .fillMaxSize()
+               .padding(innerPaddings)
+               .consumeWindowInsets(innerPaddings)
         ) {
             ChatNavHost(
                 navigationState = navigationState,
